07ccc26dc5d1233d829cb5207d8f8cb5671ccf27
broomfactory <sco20037@byui.edu>
2022-04-21 23:13:49 -0600

    w01 Assignment 1

---
+++ js/footer.js
@@ -0,0 +1,11 @@
+const copyrightSpan = document.querySelector("#copyright");
+const d = new Date();
+const year = d.getFullYear();
+
+copyrightSpan.textContent = `\u00A9 ${year} <=> Matthew Scoville`;
+
+const modifiedSpan = document.querySelector("#modifiedDate");
+const dateLastModified = document.lastModified;
+
+const lastModifiedText = `Last Updated: ${dateLastModified}`;
+modifiedSpan.textContent = lastModifiedText;    
