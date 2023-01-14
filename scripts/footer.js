418d26b0579439ef660bec3e4eda4f5a6e3dd9c2
Matthew Scoville <sco20037@byui.edu>
2023-01-13 19:57:54 -0700

    update style color

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,11 +1,5 @@
 const copyrightSpan = document.querySelector("#copyright");
-const d = new Date();
-const year = d.getFullYear();
-
-copyrightSpan.textContent = `${year}`;
+copyrightSpan.textContent = `${new Date().getFullYear()}`;
 
 const modifiedSpan = document.querySelector("#modifiedDate");
-const dateLastModified = document.lastModified;
-
-const lastModifiedText = `Last Updated: ${dateLastModified}`;
-modifiedSpan.textContent = lastModifiedText;    
+modifiedSpan.textContent = `Last Updated: ${documnet.lastModified}`;
