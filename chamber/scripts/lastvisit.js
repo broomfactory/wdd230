2b746bd8d82ef6ea072114ef6b621f4257c9bab1
broomfactory <104051666+broomfactory@users.noreply.github.com>
2023-02-18 23:15:18 -0700

    Improve last visit message

--- chamber/scripts/lastvisit.js
+++ chamber/scripts/lastvisit.js
@@ -4,7 +4,12 @@ let lastvisit = Number(window.localStorage.getItem("discover-last-visit"));
 
 if (lastvisit !== 0) {
     let dayssince = (Date.now() - lastvisit) / 86400000;
-    lastvisitdisplay.textContent = `You last visited ${dayssince.toFixed(0)} days ago. Welcome back!`;
+
+    if (dayssince.toFixed(0) === "0") {
+        lastvisitdisplay.textContent = "Your last visit to this page was earlier today.";
+    } else {
+        lastvisitdisplay.textContent = `You last visited this page ${dayssince.toFixed(0)} days ago. Welcome back!`;
+    }
 } else {
     lastvisitdisplay.textContent = "This is your first visit to this page. Welcome!";
 }
