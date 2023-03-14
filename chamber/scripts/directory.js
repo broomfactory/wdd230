b4177a58867abfc26d6342363c161ce08feb7066
broomfactory <104051666+broomfactory@users.noreply.github.com>
2023-03-13 23:59:15 -0600

    css tweaks

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -9,36 +9,40 @@ async function getDirectory() {
 
 function formatPhone(digits) {
     let nums = digits.replace(/\D/g, "")
-    return `(${nums.substring(0,2)})${nums.substring(3,5)}-${nums.substring(6,9)}`;
+    return `(${nums.substring(0,3)})${nums.substring(3,6)}-${nums.substring(6,10)}`;
 }
 
 const buildDirectory = (members) => {
     const cards = document.querySelector('div.cards'); // select the output container element
-  
+    
     members.forEach((member) => {
-      
-        let card = document.createElement('section');
-        let h2 = document.createElement('h2');
-        let logo = document.createElement('img');
-        let address = document.createElement('p');
-        let phone = document.createElement('p');
-        let siteurl = document.createElement('p');
         
-
+        let card = document.createElement('section');
+        
+        let h2 = document.createElement('h2');
         h2.textContent = member.businessname;
+        card.appendChild(h2);
+        
+        let logo = document.createElement('img');
         logo.setAttribute('src',member.imageurl);
         logo.setAttribute('alt',`${member.businessname} logo`);
         logo.setAttribute('width','200');
-
-        address.textContent = `${member.address1.trim()} ${member.address2.trim()} ${member.city.trim()} ${member.state.trim()} ${member.zipcode.trim()}`;
-      
-        siteurl.innerHTML = `<a href="${member.websiteurl}">${member.websiteurl}</a>`;
-
-        card.appendChild(h2);
         card.appendChild(logo);
+
+        let address = document.createElement('p');
+        address.textContent = `${member.address1.trim()} ${member.address2.trim()} ${member.city.trim()} ${member.state.trim()} ${member.zipcode.trim()}`;
         card.appendChild(address);
+
+        let phone = document.createElement('p');
+        phone.textContent = formatPhone(member.phone);
         card.appendChild(phone);
+        
+        let siteurl = document.createElement('p');
+        siteurl.innerHTML = `<a href="${member.websiteurl}">${member.websiteurl}</a>`;
+        siteurl.classList.toggle('wrap');
         card.appendChild(siteurl);
+  
+
 
         cards.appendChild(card);
 
