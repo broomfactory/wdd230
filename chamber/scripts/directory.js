6de7b48bb64440b1585c1590c8c840cfc1d12412
broomfactory <104051666+broomfactory@users.noreply.github.com>
2023-03-15 12:44:58 -0600

    Directory

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,4 +1,7 @@
 const url = "https://broomfactory.github.io/wdd230/chamber/data/data.json";
+const btncards = document.getElementById("dirgrid");
+const btnlist = document.getElementById("dirlist");
+const display = document.querySelector("article");
 
 async function getDirectory() {
     const response = await fetch(url);
@@ -13,7 +16,7 @@ function formatPhone(digits) {
 }
 
 const buildDirectory = (members) => {
-    const cards = document.querySelector('div.cards'); // select the output container element
+    const cards = document.querySelector('article'); // select the output container element
     
     members.forEach((member) => {
         
@@ -31,15 +34,18 @@ const buildDirectory = (members) => {
 
         let address = document.createElement('p');
         address.textContent = `${member.address1.trim()} ${member.address2.trim()} ${member.city.trim()} ${member.state.trim()} ${member.zipcode.trim()}`;
+        address.classList.add('address');
         card.appendChild(address);
 
         let phone = document.createElement('p');
         phone.textContent = formatPhone(member.phone);
+        phone.classList.add('phone');
         card.appendChild(phone);
         
         let siteurl = document.createElement('p');
         siteurl.innerHTML = `<a href="${member.websiteurl}">${member.websiteurl}</a>`;
         siteurl.classList.toggle('wrap');
+        siteurl.classList.add('url');
         card.appendChild(siteurl);
   
 
@@ -50,5 +56,17 @@ const buildDirectory = (members) => {
     })
 } 
 
+btncards.addEventListener("click", () => {
+    display.classList.add("cards");
+    display.classList.remove("list");
+});
+
+
+btnlist.addEventListener("click", () => {
+    display.classList.remove("cards");
+    display.classList.add("list");
+})
+
 
 getDirectory();
+
